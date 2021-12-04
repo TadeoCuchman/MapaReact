38e9544a28c5401bdabcafd24e385f37c0e51453
TadeoCuchman <tadeo.cuchman@gmail.com>
2021-12-04 13:39:49 -0300

    implementa icono para marcadores

---
+++ mapareact/src/Components/IconLocation.jsx
@@ -0,0 +1,14 @@
+ 
+import L from "leaflet";
+import Icon from '../icons/icon.svg'
+
+export const IconLocation = L.icon({
+  iconUrl: Icon,
+  iconRetinaUrl: Icon,
+  iconAnchor: null,
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+  iconSize: [35, 35],
+  className: "leaflet-venue-icon",
+});
