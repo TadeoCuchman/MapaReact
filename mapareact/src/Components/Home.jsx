cceba9a23da4e7400670f3b1267cfac067983ec2
TadeoCuchman <tadeo.cuchman@gmail.com>
2021-12-05 11:48:26 -0300

    Pagina de Inicio
    Esta pagina pregunta si querés compartir tu location

---
+++ mapareact/src/Components/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+
+const Home = () => {
+    const [state, setState] = useState({
+        longitude: 0,
+        latitude: 0,
+    })
+
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                setState({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                })
+            },
+            (error) =>{
+                console.log(error)
+            },
+            {
+                enableHighAccuracy : true
+            })
+    }, [])
+
+    return (
+        <div>
+            <h1>Geolocation</h1>
+            <p>latitude: {state.lat}</p>
+            <p>longitude: {state.lng}</p>
+
+            <Link state={state} to={'/map'}>See My Location</Link>
+        </div>
+    );
+}
+
+export default Home;
