7a12f21755cd132acb2e37fc9b3f8385d656c66d
TadeoCuchman <tadeo.cuchman@gmail.com>
2021-12-05 11:49:10 -0300

    Icono de location actual

--- mapareact/src/Components/IconLocation.jsx
+++ mapareact/src/Components/IconLocation.jsx
@@ -1,10 +1,22 @@
  
 import L from "leaflet";
 import Icon from '../icons/icon.svg'
+import IconCurrent from '../icons/currentLocation.svg'
 
 export const IconLocation = L.icon({
   iconUrl: Icon,
   iconRetinaUrl: Icon,
+  iconAnchor: null,
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+  iconSize: [35, 35],
+  className: "leaflet-venue-icon",
+});
+
+export const IconCurrentLocation = L.icon({
+  iconUrl: IconCurrent,
+  iconRetinaUrl: IconCurrent,
   iconAnchor: null,
   shadowUrl: null,
   shadowSize: null,
